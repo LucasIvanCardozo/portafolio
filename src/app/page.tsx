d3fdd3e5b64ad610ca53cf2959eb3228f770fb0b
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-07-29 14:01:57 -0300

    Prueba de boton para enviar mails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '@/components/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend('re_56sRMEyN_5qCqvqvGyJ64iypBd8nZd5as');
+
 export default function Home() {
+  const handleClick = () => {
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'lucasivancardozo27@gmail.com',
+      subject: 'Hello World',
+      html: '<p>Congrats on sending your <strong>first email</strong>!</p>',
+    });
+  };
+
   return (
     <>
-      <section></section>
+      <section className="h-screen text-black">
+        <button
+          onClick={handleClick}
+          type="button"
+          className="border-black border-2 m-44"
+        >
+          Send Mail
+        </button>
+      </section>
     </>
   );
 }
